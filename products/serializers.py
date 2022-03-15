b31fed178f3bebd36fd4bec116cb498b3da12410
joecoxstrong <joecoxstrong@gmail.com>
2022-03-15 16:18:16 -0400

    Added additional column to take an image url in the form of a string

--- products/serializers.py
+++ products/serializers.py
@@ -5,5 +5,5 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id','title','description','price','inventory_quantity']
+        fields = ['id','title','description','price','inventory_quantity','image_link']
         
