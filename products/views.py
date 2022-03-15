c49e2a6f74daceaad8e4e0459bfe75b11018f57e
joecoxstrong <joecoxstrong@gmail.com>
2022-03-15 14:14:49 -0400

    Updated Admin + Created superuser

--- products/views.py
+++ products/views.py
@@ -1,10 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .serializers import ProductSerializer
+from .models import Product
 
 @api_view(['GET'])
 def product_list(request):
+    products = Product.objects.all()
+
+    serializer = ProductSerializer(products, many=True)
 
 
-    return Response('ok')
+    return Response(serializer.data)
 
